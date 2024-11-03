6ac7fe9e1a4f03b5d7c7012b9726656c34af9264
Mauricio-CF <jcarvajalf2@est.ups.edu.ec>
2024-11-03 12:21:40 -0500

    V1.5

--- src/poo/PruebaAudioVisual.java
+++ src/poo/PruebaAudioVisual.java
@@ -27,19 +27,25 @@ public class PruebaAudioVisual {
         invest.add(new Investigador("Neil deGrasses Tyson"));
         invest.add(new Investigador("Steven Soter"));
         invest.add(new Investigador("Carl Sagan"));
-        
+
         List<Presentador> Present = new ArrayList<Presentador>();
         Present.add(new Presentador("Jeanette Hinostroza"));
         Present.add(new Presentador("Alejandra Boada"));
         Present.add(new Presentador("Milton Pérez"));
 
+
+        List<Host> host = new ArrayList<Host>();
+        host.add(new Host("Tomàs Fuente"));
+        host.add(new Host("Ignasi Taltavull"));
+
         // Crear instancias de las subclases
-        ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[4];
+        ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[5];
         contenidos[0] = new Pelicula("Avatar", 125, "Accion", "20th Century Studios",actores);
         contenidos[1] = new SerieDeTV("Game of Thrones", 60, "Fantasy", 8, temporadas);
         contenidos[2] = new Documental("Cosmos", 45, "Science", "Astronomy",invest);
         contenidos[3] = new NoticiasDeTV("Noticiero de la Mañana", 90, "Noticias", "Teleamazonas",Present);
-        
+        contenidos[4] = new Podcasts("La Ruina", 60, "Comedia", "se dedican a comentar, junto con un invitado al que llevan cada semana, la peor anécdota que cuente alguna de las personas que haya ido de público al programa","18-01-2023",host);
+
 
         // Mostrar los detalles de cada contenido audiovisual
         for (ContenidoAudiovisual contenido : contenidos) {
---
+++ src/uni1a/Host.java
@@ -0,0 +1,17 @@
+package uni1a;
+
+public class Host {
+    private String nombre;
+
+    public Host(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
